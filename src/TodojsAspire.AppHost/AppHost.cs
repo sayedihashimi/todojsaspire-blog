3663a951962324e475096d826c6997c098cb2fb4
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-06-16 15:49:48 -0400

    connecting front-end with the Aspire AppHost

--- src/TodojsAspire.AppHost/AppHost.cs
+++ src/TodojsAspire.AppHost/AppHost.cs
@@ -3,4 +3,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.TodojsAspire_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
+builder.AddNpmApp("todo-frontend", "../todo-frontend", "dev")
+    .WithReference(apiService)
+    .WithHttpEndpoint(targetPort: 5173);
+
 builder.Build().Run();
---
+++ src/todo-frontend/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server:{
+      port: parseInt(env.VITE_PORT),
+      proxy: {
+        // "apiservice" is the name of the API in AppHost.cs.
+        '/api': {
+          target: process.env.services__apiservice__https__0 || process.env.services__apiservice__http__0,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    build:{
+      outDir: 'dist',
+      rollupOptions: {
+        input: '.index.html'
+      }
+    }
+  }
+})
